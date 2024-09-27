c21bb9a02f24caa6ac5201bd76c4c4571863a987
Nikolai40Kuznetsov <nikolaikuznetsov@tutamail.com>
2024-09-27 23:38:15 +0300

    Задача от преподавателя

---
+++ a+b.py
@@ -0,0 +1,8 @@
+input_data = open('input.txt', 'r')
+data = input_data.read()
+data = data.split() 
+a = int(data[0]) 
+b = int(data[1]) 
+output_data = open('output1.txt', 'w')
+output_data.write(str(a + b)) 
+output_data.close()
